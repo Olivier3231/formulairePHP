0ec8395b67323ed026389bae4a7abc1e0d9681c4
Olivier Maffre <maffrolivier74@gmail.com>
2021-03-31 16:59:52 +0200

    Create thanks.php

---
+++ thanks.php
@@ -0,0 +1,5 @@
+<?php
+
+
+
+echo "Merci" . $_POST['Nom'] . " de nous avoir contacté à propos de " . $_POST['courrielsubject'] . "Un de nos conseiller vous contactera soit à l’adresse" . $_POST['courriel'] . " ou par téléphone au " . $_POST['phone'] . " dans les plus brefs délais pour traiter votre demande : " . $_POST['message'];
